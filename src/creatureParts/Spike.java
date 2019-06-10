4d49a56b5a095a699d22d7ac755f276f628994dd
BigBird15 <36560096+BigBird15@users.noreply.github.com>
2019-06-10 11:48:46 +0500

    Merge branch 'master' of C:\Users\BigBird\IdeaProjects\spore-nad with conflicts.

--- src/creatureParts/Spike.java
+++ src/creatureParts/Spike.java
@@ -1,9 +1,12 @@
 package creatureParts;
+import engine.Player;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -19,14 +22,18 @@ public class Spike extends CreaturePart {
         this.skins = new ArrayList<>();
         BufferedReader reader;
         try {
-
-            File file = new File("src\\skins\\spike.png");
-            //reader = new BufferedReader(file);
-            //skin = reader.readLine().replaceAll("but", "");
-            skins.add(new ImageIcon(ImageIO.read(file)));
+            FileReader file = new FileReader(Player.CurrentDir() + "spikes.txt");
+            reader = new BufferedReader(file);
+            skin = reader.readLine().replaceAll("but", "");
         }
         catch(IOException ioe) {
             System.err.println(ioe.toString());
+        }
+
+        try {
+            this.skins.add(new ImageIcon(ImageIO.read(new File(skin))));
+        }catch(IOException ioe){
+            System.out.println("can't load spike sprite");
         }
     }
 }
